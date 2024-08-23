e6e87817b959cd5762bc8746b657e29a51c5ba7c
Kymemia <kimemiachristopherr@gmail.com>
2024-08-23 16:24:49 +0300

    error fixes

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -20,18 +20,28 @@ def validUTF8(data):
             return False
         elif byte < 0b11100000:
             if (value + 1 >= len(data) or
-                data[value + 1] < 0b10000000 or
-                data[value + 1] >= 0b11000000):
+                    data[value + 1] < 0b10000000 or
+                    data[value + 1] >= 0b11000000):
                 return False
             value += 2
         elif byte < 0b11110000:
             if (value + 2 >= len(data) or
-                data[value + 1] < 0b10000000 or
-                data[value + 1] >= 0b11000000 or
-                data[value + 2] < 0b10000000 or
-                data[value + 2] >= 0b11000000):
+                    data[value + 1] < 0b10000000 or
+                    data[value + 1] >= 0b11000000 or
+                    data[value + 2] < 0b10000000 or
+                    data[value + 2] >= 0b11000000):
                 return False
             value += 3
+        elif byte < 0b11111000:
+            if (value + 3 >= len(data) or
+                    data[value + 1] < 0b10000000 or
+                    data[value + 1] >= 0b11000000 or
+                    data[value + 2] < 0b10000000 or
+                    data[value + 2] >= 0b11000000 or
+                    data[value + 3] < 0b10000000 or
+                    data[value + 3] >= 0b11000000):
+                return False
+            value += 4
         else:
             return False
 
