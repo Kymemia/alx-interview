f48d9b8d427ee8ec7001d5815328496cad9a678d
Kymemia <kimemiachristopherr@gmail.com>
2024-09-08 16:33:34 +0300

    staw wars api request module fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,36 @@
 #!/usr/bin/node
 const argv = process.argv;
-const axios = require('axios');
+const request = require('request');
 
 const urlFilm = 'https://swapi-api.hbtn.io/api/films/';
 const urlMovie = `${urlFilm}${argv[2]}/`;
 
-async function fetchMovieAndCharacters () {
-  try {
-    const { data: fbody } = await axios.get(urlMovie);
-    const characters = fbody.characters;
+function fetchMovieAndCharacters () {
+  request(urlMovie, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const fbody = JSON.parse(body);
+      const characters = fbody.characters;
 
-    if (characters && characters.length > 0) {
-      const characterPromises = characters.map((url) => axios.get(url));
-      const responses = await Promise.all(characterPromises);
-
-      responses.forEach((response) => {
-        console.log(response.data.name);
-      });
+      if (characters && characters.length > 0) {
+        let index = 0;
+        const fetchCharacter = () => {
+          if (index < characters.length) {
+            request(characters[index], (error, response, body) => {
+              if (!error && response.statusCode === 200) {
+                const character = JSON.parse(body);
+                console.log(character.name);
+                index++;
+                fetchCharacter();
+              }
+            });
+          }
+        };
+        fetchCharacter();
+      }
+    } else {
+      console.error('error:', error || `Status Code: ${response.statusCode}`);
     }
-  } catch (error) {
-    console.error('error:', error);
-  }
+  });
 }
 
 fetchMovieAndCharacters();
