7c118dd0254e2589655859495a268681c7a2de53
Kymemia <kimemiachristopherr@gmail.com>
2024-09-19 11:32:42 +0300

    change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+function that returns the fewest number of coins needed
+to meet a given amount, total
+Solves the coin change problem using dynamic programming
+"""
+
+
+def makeChange(coins, total):
+    """
+    function that solves the coin change problem
+    """
+    r = [float("infinity")] * (total + 1)
+    r[0] = 0
+
+    for coin in coins:
+        for x in range(coin, total + 1):
+            r[x] = min(r[x], r[x - coin] + 1)
+
+    return r[total] if r[total] != float("infinity") else - 1
